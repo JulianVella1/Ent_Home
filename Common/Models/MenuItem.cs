266c76b1c23dcb5b684ef779c0f59ad620a0dd26
JulianVella1 <114913424+JulianVella1@users.noreply.github.com>
2026-02-21 13:33:36 +0100

    DB context and interface

---
+++ Common/Models/MenuItem.cs
@@ -0,0 +1,42 @@
+﻿using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    internal class MenuItem : IItemValidating
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required, MaxLength(150)]
+        public string Title { get; set; } = string.Empty;
+        public double Price { get; set; }
+        [Required, MaxLength(30)]
+        [ForeignKey("Restaurant")]
+        public int RestId {get; set; }
+        public virtual Restaurant? Restaurant { get; set; }
+        public bool Status { get; set; } = false;
+        public string? ImagePath { get; set; }
+
+
+        public string GetCardPartial()
+        {
+            return "_MenuItemCard";
+        }
+
+        public List<string> GetValidators()
+        {
+            var validators= new List<string>();
+
+            if (Restaurant != null && !string.IsNullOrWhiteSpace(Restaurant.OwnerEmail)) { 
+                validators.Add(Restaurant.OwnerEmail);
+            }
+            return validators;
+        }
+    }
+}
---
+++ Common/Models/Restaurant.cs
@@ -0,0 +1,33 @@
+﻿using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    internal class Restaurant : IItemValidating
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public string OwnerEmail { get; set; } = string.Empty;
+        public bool Status { get; set; } = false;
+        public string? ImagePath { get; set; }
+        public virtual ICollection<MenuItem> MenuItems { get; set; }
+        public string GetCardPartial()
+        {
+            return "_RestaurantCard";
+        }
+
+        public List<string> GetValidators()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
